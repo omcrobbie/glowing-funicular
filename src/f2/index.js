56a3e34fd0ece840732cfc53ed3020c1aaffef74
distres117 <omcrobbie@gmail.com>
2021-07-16 20:14:40 -0400

    init

---
+++ src/f2/index.js
@@ -0,0 +1,12 @@
+function fibonacci2(num, memo) {
+    memo = memo || {};
+  
+    if (memo[num]) {
+        return memo[num];
+    }
+    if (num <= 1) {
+        return 1;
+    }
+  
+    return memo[num] = fibonacci(num - 1, memo) + fibonacci(num - 2, memo);
+  }
