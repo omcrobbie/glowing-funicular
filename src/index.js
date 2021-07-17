82d58514ca02b3cd18929bc2a0ea5c311a59037b
distres117 <omcrobbie@gmail.com>
2021-07-17 19:41:35 -0400

    works

--- src/index.js
+++ src/index.js
@@ -1,44 +1,27 @@
+const parseDiff = require("parse-diff");
+const fs = require("fs");
+const path = require("path");
+const _ = require('lodash');
 
-const lineReader = require('line-reader');
-const parseDiff = require('parse-diff');
-const fs = require('fs');
-const path = require('path');
-
-
-/**
- * @description build a valid diff patch using an input obj
- * @param {any} diffObj
- * @returns {any}
- */
-function buildDiff(diffObj) {
-    //TODO
-}
-/**
- * @description finds a matching line in the input file
- * @param {regex} lineRegex
- * @param {any} inputLine
- * @returns {number} lineNumber
- */
-function findLine(lineRegex, inputLine) {
-    // TODO
-}
-/**
- * @description scans input file using a the diffObject
- * @param {any} file
- * @param {any} diffObj
- * @returns {any}
- */
-function scanInputFile(file, diffObj) {
-
-}
-module.exports = function(patchFile) {
-    const diffObj = parseDiff(patchFile);
-    diffObj.forEach(chunk => {
-        const p = path.join(__dirname, path.relative(__dirname, chunk.from))
-        console.log(p)
-        if (fs.existsSync(p)) {
-            console.log(chunk.changes)
-        }
-    })
-
-}
+module.exports = function (patchFile) {
+  const diffObj = parseDiff(patchFile);
+  diffObj.forEach((file) => {
+    const fileName = path.join(__dirname, path.relative(__dirname, file.from));
+    if (fs.existsSync(fileName)) {
+        const fileData = fs.readFileSync(fileName, { encoding: "utf-8" });
+        const lines = fileData.split('\n');
+      file.chunks.forEach(chunk => {
+          const groups = _.groupBy(chunk.changes, 'ln');
+          console.log(groups);
+          delete groups[undefined];
+          Object.keys(groups).map(k => groups[k]).forEach(([del, add]) => {
+              const match = del.content.substr(1);
+              const replace = add.content.substr(1);
+              const matched = lines.find(ln => ln === match);
+              const lineNum = lines.indexOf(matched);
+              lines[lineNum] = replace;
+          })
+      })
+      fs.writeFileSync(fileName, lines.join('\n'));
+  });
+};
