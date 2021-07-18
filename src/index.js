e056c75bf6443d1e5ed62c581be1cdca496cc5b0
distres117 <omcrobbie@gmail.com>
2021-07-18 10:56:55 -0400

    refactor

--- src/index.js
+++ src/index.js
@@ -1,30 +1,26 @@
-const parseDiff = require("parse-diff");
 const fs = require("fs");
 const path = require("path");
 const _ = require("lodash");
 
-module.exports = function (patchFile) {
-  const diffObj = parseDiff(patchFile);
-  diffObj.forEach((file) => {
-    const fileName = path.join(__dirname, path.relative(__dirname, file.from));
-    if (fs.existsSync(fileName)) {
-      const fileData = fs.readFileSync(fileName, { encoding: "utf-8" });
-      const lines = fileData.split("\n");
-      file.chunks.forEach((chunk) => {
-        const groups = _.groupBy(chunk.changes, "ln");
-        console.log(groups);
-        delete groups[undefined];
-        Object.keys(groups)
-          .map((k) => groups[k])
-          .forEach(([del, add]) => {
-            const match = del.content.substr(1);
-            const replace = add.content.substr(1);
-            const matched = lines.find((ln) => ln === match);
-            const lineNum = lines.indexOf(matched);
-            lines[lineNum] = replace;
-          });
+function handleChunks(chunks, lines) {
+  chunks.forEach((chunk) => {
+    const groups = _.groupBy(chunk.changes, "ln");
+    delete groups[undefined];
+    Object.keys(groups)
+      .map((k) => groups[k])
+      .forEach(([del, add]) => {
+        const match = del.content.substr(1);
+        const replace = add.content.substr(1);
+        const matched = lines.find((ln) => ln === match);
+        const lineNum = lines.indexOf(matched);
+        lines[lineNum] = replace;
       });
-      fs.writeFileSync(fileName, lines.join("\n"));
-    }
   });
+  return lines;
+  
 };
+
+function handleUpdate(changes) {
+  // iterate over changes
+}
+module.exports = { handleChunks };
--- test/index.test.js
+++ test/index.test.js
@@ -1,12 +1,37 @@
-const fs = require('fs');
-const path = require('path');
-const converter = require('../src');
+const fs = require("fs");
+const path = require("path");
+const { handleChunks } = require("../src");
+const parseDiff = require("parse-diff");
+const { toMatchFile } = require('jest-file-snapshot');
 
-let patch;
-beforeAll(() => {
-    patch = fs.readFileSync(path.join(__dirname, '../p.patch'), { encoding: 'utf8'});
-})
+expect.extend({ toMatchFile });
+function testFn(patchName) {
+  const patch = fs.readFileSync(path.join(__dirname, `../${patchName}`), {
+    encoding: "utf8",
+  });
+  const diffObj = parseDiff(patch);
+  let lines = [];
+  diffObj.forEach((file) => {
+    const fileName = path.join(__dirname, path.relative(__dirname, file.from));
+    if (fs.existsSync(fileName)) {
+      const fileData = fs.readFileSync(fileName, { encoding: "utf-8" });
+      lines = lines.concat(fileData.split("\n"));
+      handleChunks(file.chunks, lines);
+    }
+  });
+  expect(lines.join('\n')).toMatchFile();
+}
 
-test('should do stuff', () => {
-    converter(patch);
-})
+test("should handle mixed chunks", () => {
+  testFn('p.patch');
+});
+// test('should handle substractive chunks', () => {
+
+// })
+// test('should handle mixed chunks', () => {
+
+// })
+// test('should handle complex chunks', () => {
+
+// })
+
